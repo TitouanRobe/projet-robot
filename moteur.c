fce52568d0fb64ce596c9a04d46234af307680fd
ABDELWAHED Wissam <wissam.abdelwahed@groupe-esigelec.org>
2025-09-19 16:18:40 +0200

    V2 moteur, roule droit mais ne réalise pas la bonne distance

--- moteur.c
+++ moteur.c
@@ -5,15 +5,18 @@
 #define L_MOTOR_DIR BIT1
 #define L_MOTOR_PWM BIT2
 #define aze  (100.0 / 174.0)
-#define DISTG_VOULU 300.0
+#define DISTG_VOULU (150.0)
 #define TICSG_VOULU (DISTG_VOULU/aze)
 
+#define RATIO (1)
 #define BTN_S2 BIT3
+#define VITESSEG (54)
+#define VITESSED (50)
 
 #define R_MOTOR_PWM BIT4
 #define R_MOTOR_DIR BIT5
 #define R_ODOM BIT3
-#define DISTD_VOULU 300.0
+#define DISTD_VOULU (150.0)
 #define TICSD_VOULU (DISTD_VOULU/aze)
 
 volatile int i;
@@ -41,20 +44,20 @@ __interrupt void port2_isr(void) {
   if ((P2IFG & L_ODOM) == L_ODOM){
     l_cpt++;
     P2IES ^= L_ODOM;
-  
-    if (l_cpt>TICSG_VOULU){
-      TA1CCR1 = 0;
-    }
     P2IFG &= ~(L_ODOM);
+    if (r_cpt>TICSG_VOULU){
+      TA1CCR1 = 0;
+      TA1CCR2 = 0;
+    }
   }
   if ((P2IFG & R_ODOM) == R_ODOM){
     r_cpt++;
     P2IES ^= R_ODOM;
-  
-    if (r_cpt>TICSD_VOULU){
+    P2IFG &= ~(R_ODOM);
+    if (r_cpt>TICSG_VOULU){
+      TA1CCR1 = 0;
       TA1CCR2 = 0;
     }
-    P2IFG &= ~(R_ODOM);
   }
 }
 
@@ -62,12 +65,9 @@ __interrupt void port2_isr(void) {
 void left_init(void) {
   P2OUT &= ~(L_MOTOR_DIR);
 
-
   P1DIR &= ~BTN_S2;
 
-
   P2DIR |= (L_MOTOR_PWM | L_MOTOR_DIR);
-
 
   P2SEL |= (L_MOTOR_PWM);
   P2SEL &= ~(L_MOTOR_DIR);
@@ -76,16 +76,10 @@ void left_init(void) {
 
 
 void left_timer(void) {
-  TA1CCR0 = 1000;
-  TA1CCR1 = 250;    
-
-
+  TA1CTL |= TASSEL_2 | MC_1 |ID_0;  
   TA1CCTL1 |= OUTMOD_7;
-  TA1CTL |= TASSEL_2 | MC_1 |
-            ID_0;  
-                   
-  TA1CTL &= ~MC_0;
-  TA1CTL |= MC_1 | ID_2;
+  TA1CCR0 = 100;
+  TA1CCR1 = VITESSEG;  
 }
 
 
@@ -93,9 +87,7 @@ void left_odom(void){
   P2SEL &= ~(L_ODOM);
   P2SEL2 &= ~(L_ODOM);
 
-
   P2DIR &= ~(L_ODOM);
-
 
   P2IE |= L_ODOM;
   P2IES |= L_ODOM; //front descendant
@@ -112,12 +104,9 @@ void left_wheel(void) {
 void right_init(void) {
   P2OUT |= (R_MOTOR_DIR);
 
-
   P1DIR &= ~BTN_S2;
 
-
   P2DIR |= (R_MOTOR_PWM | R_MOTOR_DIR);
-
 
   P2SEL |= (R_MOTOR_PWM);
   P2SEL &= ~(R_MOTOR_DIR);
@@ -126,23 +115,15 @@ void right_init(void) {
 
 
 void right_timer(void) {
-  TA1CCR0 = 1000;
-  TA1CCR2 = 250;
-
-
   TA1CCTL2 |= OUTMOD_7;
-  TA1CTL |= TASSEL_2 | MC_1 | ID_0;
-  TA1CTL &= ~MC_0;
-  TA1CTL |= MC_1 | ID_2;
+  TA1CCR2 = VITESSED;
 }
 
 void right_odom(void){
   P2SEL &= ~(R_ODOM);
   P2SEL2 &= ~(R_ODOM);
 
-
   P2DIR &= ~(R_ODOM);
-
 
   P2IE |= R_ODOM;
   P2IES |= R_ODOM; //front descendant
@@ -163,10 +144,8 @@ void bouton(void){
   P1REN |= BTN_S2;
   P1OUT |= BTN_S2;
 
-
   P1IE |= BTN_S2;
   P1IES |= BTN_S2;
-
 
   P1IFG &= ~(BTN_S2);
 }
@@ -176,16 +155,12 @@ int main(void) {
   BCSCTL1 = CALBC1_1MHZ;
   DCOCTL = CALDCO_1MHZ;
 
-
-
   bouton();
 
   left_wheel();
   right_wheel();
 
-
   __enable_interrupt();
-  
 
   while (1){
 
